1661cce0d2f2f5d6df19ce2ca18f939a8612f1f8
ryu <fbqudrlf09@naver.com>
2024-10-28 05:08:51 +0900

    힙 자료구조 구현

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,16 @@ import _03_DoublyLinkedList.DLinkedList;
 import _04_Stack.Stack;
 import _05_ArrayQueue.ArrayQueue;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList
+        String str = "you must know.";
 
+        System.out.println(str.length());
     }
 }
---
+++ src/_09_Heap/Heap.java
@@ -0,0 +1,220 @@
+package _09_Heap;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+public class Heap<E> {
+
+    private final Comparator<? super E> comparator;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private int size;
+
+    private Object[] array;
+
+    public Heap() {
+        this(null);
+    }
+
+    public Heap(Comparator<? super E> comparator) {
+        this.array = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+        this.comparator = comparator;
+    }
+
+    public Heap(int capacity) {
+        this(capacity, null);
+    }
+
+    public Heap(int capacity, Comparator<? super E> comparator) {
+        this.array = new Object[capacity];
+        this.size = 0;
+        this.comparator = comparator;
+    }
+
+    // 부모 인덱스 찾기
+    private int getParent(int index) {
+        return index/2;
+    }
+
+    // 왼쪽 자식 인덱스 찾기
+    private int getLeftChild(int index) {
+        return index * 2;
+    }
+
+    // 오른쪽 자식 인덱스 찾기
+    private int getRightChild(int index) {
+        return index * 2 + 1;
+    }
+
+    private void resize(int newCapacity) {
+        Object[] newArray = new Object[newCapacity];
+
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+
+        this.array = null;
+        this.array = newArray;
+    }
+
+    public void add(E value) {
+        if (size + 1 == array.length) {
+            resize(array.length * 2);
+        }
+
+        siftUp(size + 1, value);
+        size++;
+    }
+
+    private void siftUp(int idx, E target) {
+        if (comparator != null) {
+            siftUpComparator(idx, target, comparator);
+        } else {
+            siftUpComparable(idx, target);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftUpComparable(int idx, E target) {
+        Comparable<? super E> comp = (Comparable<? super E>) target;
+
+        while (idx > 1) {
+            int parent = getParent(idx);
+            Object parentVal = array[parent];
+
+            if (comp.compareTo((E) parentVal) >= 0) {
+                break;
+            }
+
+            array[idx] = parentVal;
+            idx = parent;
+        }
+        array[idx] = comp;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftUpComparator(int idx, E target, Comparator<? super E> comparator) {
+        while (idx > 1) {
+            int parent = getParent(idx);
+            Object parentVal = array[parent];
+
+            if (comparator.compare(target, (E) parentVal) >= 0) {
+                break;
+            }
+
+            array[idx] = parentVal;
+            idx = parent;
+        }
+        array[idx] = target;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E remove() {
+        if (array[1] == null) {
+            throw new NoSuchElementException();
+        }
+
+        E result = (E) array[1];
+        E target;
+
+        if (size == 1) {
+            target = null;
+        } else {
+            target = (E) array[size];
+        }
+
+        siftDown(1, target);
+
+        return result;
+    }
+
+    private void siftDown(int idx, E target) {
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftDownComparator(int idx, E target, Comparator<? super E> comparator) {
+        array[idx] = null;
+        size--;
+
+        int parent = idx;
+        int child;
+
+        while ((child = getLeftChild(parent)) >= size) {
+            int right = getRightChild(parent);
+
+            Object childVal = array[child];
+
+            if (right <= size && comparator.compare((E) childVal, (E) array[right]) > 0) {
+                child = right;
+                childVal = array[child];
+            }
+
+            if (comparator.compare(target, (E) childVal) <= 0) {
+                break;
+            }
+
+            array[parent] = childVal;
+            parent = child;
+        }
+        array[parent] = target;
+
+        if (array.length > DEFAULT_CAPACITY && size < array.length / 4) {
+            resize(Math.max(DEFAULT_CAPACITY, array.length / 2));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftDownComparable(int idx, E target) {
+        array[idx] = null;
+        size--;
+
+        int parent = idx;
+        int child;
+
+        while ((child = getLeftChild(parent)) >= size) {
+            int right = getRightChild(parent);
+
+            Object childVal = array[child];
+
+            if (right <= size && comparator.compare((E) childVal, (E) array[child]) > 0) {
+                child = right;
+                childVal = array[child];
+            }
+
+            if (comparator.compare(target, (E) childVal) <= 0) {
+                break;
+            }
+
+            array[parent] = childVal;
+            parent = child;
+        }
+        array[parent] = target;
+
+        if (array.length > DEFAULT_CAPACITY && size < array.length / 4) {
+            resize(Math.max(DEFAULT_CAPACITY, array.length / 2));
+        }
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E peek() {
+        if (array[1] == null) {
+            throw new NoSuchElementException();
+        }
+        return (E)array[1];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Object[] toArray() {
+        return Arrays.copyOf(array, size + 1);
+    }
+}
